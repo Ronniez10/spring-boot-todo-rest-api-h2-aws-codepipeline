0e1a1e10a8e2729a77ff7250b3e2860847fd7f34
Neelav <bronnieneelav@gmail.com>
2020-06-07 02:38:05 +0530

    Changed hello-world mapping

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -10,7 +10,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World Code Pipeline";
+		return "Hello World Code Pipeline Version 1";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
